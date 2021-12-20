738e0e9356eddeaaff67c3056031b5c41838fa97
Shohrat-Code <shohrat@code.edu.az>
2021-12-20 21:33:24 +0400

    Codes

---
+++ azen/azen/Models/Subscribe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace azen.Models
+{
+    public class Subscribe
+    {
+        [Key]
+        public int Id { get; set; }
+       
+        [MaxLength(50)]
+        public string Email { get; set; }
+        
+        public DateTime CreatedDate { get; set; }
+    }
+}
